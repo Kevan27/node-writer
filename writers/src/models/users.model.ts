c9d0988892d95d993b0e5c75de2b320cade4cfc7
Kevan27 <kevanhingarajiya19@gmail.com>
2024-02-27 00:06:13 +0530

    Craete the interface and model for the user

---
+++ writers/src/models/users.model.ts
@@ -0,0 +1,14 @@
+import { model, Schema, Model, Document } from 'mongoose';
+import UserInterface from '../types/user.type';
+
+const userSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const UserModel: Model<UserInterface & Document> = model<UserInterface & Document>('User', userSchema);
+
+export default UserModel;
